4f54ecca0842be691f05e3252c1dc001991f8aef
unknown <carlos.sala.munoz@gmail.com>
2024-11-26 22:46:09 -0300

    ahora se ingresa xml en jetpack compose en interoperabilityActivity

--- app/src/main/java/com/example/layouts/InteroperabilityActivity.kt
+++ app/src/main/java/com/example/layouts/InteroperabilityActivity.kt
@@ -1,12 +1,22 @@
 package com.example.layouts
 
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.FabPosition.Companion.Center
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.layouts.databinding.ActivityInteroperabilityBinding
@@ -21,6 +31,7 @@ class InteroperabilityActivity : AppCompatActivity() {
         binding = ActivityInteroperabilityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Jetpack Compose since AndroidViews (xml)
         val composeView = findViewById<ComposeView>(R.id.compose_view)
         composeView.setContent {
             HelloCompose()
@@ -35,7 +46,21 @@ class InteroperabilityActivity : AppCompatActivity() {
     }
 }
 
+@Preview(showBackground = true)
 @Composable
 fun HelloCompose(modifier: Modifier = Modifier) {
-    Text("hello compose")
+    Column(Modifier.fillMaxSize()) {
+        Text(
+            text = "hello sin jetpack compose",
+            textAlign = TextAlign.Center,
+           // modifier = Modifier.wrapContentWidth(Alignment.CenterHorizontally)
+        )
+        // Android View (xml) since Jetpack Compose)
+        AndroidView(factory = {
+            TextView(it).apply {
+                text = "hello xml since jetpack compose"
+                gravity = Gravity.CENTER_HORIZONTAL
+            }
+        })
+    }
 }
